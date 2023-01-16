fef757f0907f8a1a2c588e908d4a4b078a33bedb
frttnk <firattonak@gmail.com>
2023-01-15 23:46:44 -0500

    Exception_Handling

---
+++ Exception_Handling/Exception_Handling/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+
+/*
+try
+{
+    int first = 0;
+    int second = 0;
+
+    int result = first / second;
+}
+catch (Exception excp)
+{
+    Console.WriteLine("Divide by zero error :\n"+excp.ToString());
+}
+finally
+{
+    Console.WriteLine("\nWe got an error and need to change the logic here");
+}
+*/
+
+
+try
+{
+    DivideZero();
+}
+catch (Exception excp)
+{
+    Console.WriteLine(excp.StackTrace);
+}
+
+void DivideZero()
+{
+    try
+    {
+        int first = 0;
+        int second = 0;
+
+        int result = first / second;
+    }
+    catch (Exception excp)
+    {
+        throw excp;
+    }
+}
